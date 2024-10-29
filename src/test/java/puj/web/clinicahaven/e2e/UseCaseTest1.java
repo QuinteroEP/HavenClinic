b140ec8c1bd4e4567a200356db59b05de486d70c
johny2004 <jonathanjurados@gmail.com>
2024-10-29 10:10:48 -0500

    cambios caso 1

--- src/test/java/puj/web/clinicahaven/e2e/UseCaseTest1.java
+++ src/test/java/puj/web/clinicahaven/e2e/UseCaseTest1.java
@@ -63,6 +63,154 @@ public class UseCaseTest1 {
         WebElement btnLogIn = driver.findElement(By.id("LogIn"));
         btnLogIn.click();
     
+        
+        // Esperar a que el popup aparezca
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Loginemail")));
+    
+        // inicio incorrecto/info incorrecta
+        WebElement inputLoginCorreo = driver.findElement(By.id("Loginemail"));
+        inputLoginCorreo.sendKeys("qwasdfge@m.c");
+    
+        WebElement inputLoginpass = driver.findElement(By.id("Loginpsw"));
+        inputLoginpass.sendKeys("abdefrtgyhuc");
+    
+        //  checkbox
+        WebElement checkBox = driver.findElement(By.id("check-box"));
+        checkBox.click();
+    
+        // Attempt to log in/incorrectamente
+        WebElement btnLoginCuenta = driver.findElement(By.id("cancel"));
+        btnLoginCuenta.click();
+    
+        /*
+    
+        // 2. Iniciar Sesion correctamente
+        // Clear the previous inputs
+        inputLoginCorreo.clear();
+        inputLoginpass.clear();
+    
+        // informacion correcta
+        inputLoginCorreo.sendKeys("qwe@m.c");
+        inputLoginpass.sendKeys("abc");
+    
+        // Attempt to log in again
+        btnLoginCuenta.click();
+    
+        // verificar login
+       // wait.until(ExpectedConditions.presenceOfElementLocated(By.id())); // Replace with an element that appears after successful login
+    */
+
+    //2. inciar sesion correctamente
+    //desplegar popup
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("LogIn")));
+    WebElement btnLogIn2 = driver.findElement(By.id("LogIn"));
+    btnLogIn2.click();
+
+    // Esperar a que el popup aparezca
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Loginemail")));
+    
+    // inicio incorrecto/info incorrecta
+    WebElement inputLoginCorreo2 = driver.findElement(By.id("Loginemail"));
+    inputLoginCorreo2.sendKeys("qwe@m.c");
+
+    WebElement inputLoginpass2 = driver.findElement(By.id("Loginpsw"));
+    inputLoginpass2.sendKeys("abc");
+
+    //  checkbox
+    WebElement checkBox2 = driver.findElement(By.id("check-box"));
+    checkBox2.click();
+
+    // Attempt to log in/correctamente
+    WebElement btnLoginCuentaCorrecta = driver.findElement(By.id("btnloginCuenta"));
+    btnLoginCuentaCorrecta.click();
+
+    //3.ingresar nuevo cliente
+    //entrar a lista de clientes
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnlistaClientes")));
+    WebElement btnListaClientes = driver.findElement(By.id("btnlistaClientes"));
+    btnListaClientes.click();
+
+    //entrar a forms agregar un nuevo cliente
+
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAgregarCliente")));
+    WebElement btnAgregarCliente = driver.findElement(By.id("btnagregarCliente"));
+    btnAgregarCliente.click();
+
+     // Llenar informacion nuevo cliente
+     wait.until(ExpectedConditions.presenceOfElementLocated(By.id("name")));
+
+     WebElement inputNameCliente = driver.findElement(By.id("name"));
+     inputNameCliente.sendKeys("Juan Perez");
+ 
+     WebElement inputCedulaCliente = driver.findElement(By.id("cedula"));
+     inputCedulaCliente.sendKeys(Keys.BACK_SPACE);
+     inputCedulaCliente.sendKeys("98765");
+ 
+     WebElement inputCelularCliente = driver.findElement(By.id("celular"));
+     inputCelularCliente.sendKeys(Keys.BACK_SPACE);
+     inputCelularCliente.sendKeys("987654");
+ 
+     WebElement inputCorreoCliente = driver.findElement(By.id("correo"));
+     inputCorreoCliente.sendKeys("juan.perez@example.com");
+ 
+     WebElement inputContrasenaCliente = driver.findElement(By.id("contrasena"));
+     inputContrasenaCliente.sendKeys("password123");
+ 
+     // Submit the form
+     WebElement submitButton = driver.findElement(By.id("SubmitCliente"));
+     submitButton.click();
+ 
+    //4. Agregar una mascota nueva asociandola al nuevo cliente
+
+    //entrar al forms agregar nueva mascota
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnAgregarMascota")));
+    WebElement btnagregarMascota = driver.findElement(By.id("btnagregarMascota"));
+    btnagregarMascota.click();
+
+    // Llenar informacion nueva mascota
+        // Fill out the form
+        WebElement inputName = driver.findElement(By.id("name"));
+        inputName.sendKeys("Firulais");
+    
+        WebElement inputAge = driver.findElement(By.id("age"));
+        inputAge.sendKeys("5");
+    
+        WebElement inputBreed = driver.findElement(By.id("breed"));
+        inputBreed.sendKeys("Labrador");
+    
+        WebElement inputCedulaDueno = driver.findElement(By.id("cedulaDueno"));
+        inputCedulaDueno.sendKeys(Keys.BACK_SPACE);
+        inputCedulaDueno.sendKeys("98765");
+    
+        WebElement inputGender = driver.findElement(By.id("gender"));
+        inputGender.sendKeys("Macho");
+    
+        WebElement inputCondition = driver.findElement(By.id("condition"));
+        inputCondition.sendKeys("Sano");
+    
+        WebElement inputDescription = driver.findElement(By.id("description"));
+        inputDescription.sendKeys("Muy amigable");
+    
+        WebElement inputUrl = driver.findElement(By.id("url"));
+        inputUrl.sendKeys("http://example.com/firulais.jpg");
+
+        // Submit the form
+            // Submit the form      // Wait until the button is clickable
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("submitMascotaBtn")));
+    WebElement submitButtonMascota = driver.findElement(By.id("submitMascotaBtn"));
+    submitButtonMascota.click();
+
+//5.El cliente verifica que se realizo correctamente el inicio de sesion
+//salir de la sesion de veterinario
+wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnCerrarSesion")));
+WebElement btnCerrarSesion = driver.findElement(By.id("btnCerrarSesion"));
+btnCerrarSesion.click();
+
+
+
+
+
        }
 
 
